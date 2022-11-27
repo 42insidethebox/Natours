3b96e507f2524ee440dd663d4ac4ba716f87e2e6
ratchetandcunt <42insidethebox@gmail.com>
2022-11-27 09:39:03 +0100

    commeeet

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongoose
 
 // 4 - START SERVER
 const port = process.env.PORT || 3000;
-
+console.log('this is the port', port);
 const server = app.listen(port, () => {
   // console.log(`App running on port ${port}...`);
 });
